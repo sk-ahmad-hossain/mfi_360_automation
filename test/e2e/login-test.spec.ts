d40a464066320a656072527d92f6762a4ced6c84
Saifi Azmi <saifi.azmi@icraanalytics.com>
2025-08-07 13:01:43 +0530

    Comment removed

--- test/e2e/login-test.spec.ts
+++ test/e2e/login-test.spec.ts
@@ -2,12 +2,6 @@ import { test } from '@playwright/test';
 import { Login } from '@pages/login-page';
 
 test.skip('login test', async ({ page }, testInfo) => {
-  console.log(testInfo.project.metadata);
-  
   const login = new Login(page, testInfo);
   await login.login();
-  // wait for the page to load
-
-  //validate home page
-  //await expect(page).toHaveURL(/.*home/);
 });
