a8bdebfe2fd3cfe778ca8f9b521150a5e39afb4e
Ahmad <ahmad.hossain@icraanalycits.com>
2025-08-18 12:23:55 +0530

    login spec deleted

--- test/e2e/login-test.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test } from '@playwright/test';
-import { Login } from '@pages/login-page';
-
-test.skip('login test', async ({ page }, testInfo) => {
-  const login = new Login(page, testInfo);
-  await login.login();
-});
